34a7274a228264ac60f2173c8e6e9f259ea1d642
Joshua Schroijen <Joshua.Schroijen@kuleuven.be>
2022-08-15 04:57:00 +0200

    Implement double calibration experiment

--- demo.py
+++ demo.py
@@ -3,39 +3,22 @@ import os
 import torch
 import torchvision as tv
 from torch.utils.data.sampler import SubsetRandomSampler
+import statistics
+
 from models import DenseNet
 from temperature_scaling import ModelWithTemperature
 
-
 def demo(data, save, depth=40, growth_rate=12, batch_size=256):
-    """
-    Applies temperature scaling to a trained model.
-
-    Takes a pretrained DenseNet-CIFAR100 model, and a validation set
-    (parameterized by indices on train set).
-    Applies temperature scaling, and saves a temperature scaled version.
-
-    NB: the "save" parameter references a DIRECTORY, not a file.
-    In that directory, there should be two files:
-    - model.pth (model state dict)
-    - valid_indices.pth (a list of indices corresponding to the validation set).
-
-    data (str) - path to directory where data should be loaded from/downloaded
-    save (str) - directory with necessary files (see above)
-    """
-    # Load model state dict
     model_filename = os.path.join(save, 'model.pth')
     if not os.path.exists(model_filename):
         raise RuntimeError('Cannot find file %s to load' % model_filename)
     state_dict = torch.load(model_filename)
 
-    # Load validation indices
     valid_indices_filename = os.path.join(save, 'valid_indices.pth')
     if not os.path.exists(valid_indices_filename):
         raise RuntimeError('Cannot find file %s to load' % valid_indices_filename)
     valid_indices = torch.load(valid_indices_filename)
 
-    # Regenerate validation set loader
     mean = [0.5071, 0.4867, 0.4408]
     stdv = [0.2675, 0.2565, 0.2761]
     test_transforms = tv.transforms.Compose([
@@ -46,7 +29,6 @@ def demo(data, save, depth=40, growth_rate=12, batch_size=256):
     valid_loader = torch.utils.data.DataLoader(valid_set, pin_memory=True, batch_size=batch_size,
                                                sampler=SubsetRandomSampler(valid_indices))
 
-    # Load original model
     if (depth - 4) % 3:
         raise Exception('Invalid depth')
     block_config = [(depth - 4) // 6 for _ in range(3)]
@@ -57,31 +39,13 @@ def demo(data, save, depth=40, growth_rate=12, batch_size=256):
     ).cuda()
     orig_model.load_state_dict(state_dict)
 
-    # Now we're going to wrap the model with a decorator that adds temperature scaling
-    model = ModelWithTemperature(orig_model)
+    diffs = []
+    for _ in range(100):
+      old_ece = model.set_temperature(valid_loader)
+      new_ece = model.set_temperature(valid_loader)
+      diffs.append((new_ece - old_ece))
 
-    # Tune the model temperature, and save the results
-    model.set_temperature(valid_loader)
-    model_filename = os.path.join(save, 'model_with_temperature.pth')
-    torch.save(model.state_dict(), model_filename)
-    print('Temperature scaled model sved to %s' % model_filename)
-    print('Done!')
-
+    print(f"Mean ECE difference after calibrating twice: {statistics.mean(diffs)}")
 
 if __name__ == '__main__':
-    """
-    Applies temperature scaling to a trained model.
-
-    Takes a pretrained DenseNet-CIFAR100 model, and a validation set
-    (parameterized by indices on train set).
-    Applies temperature scaling, and saves a temperature scaled version.
-
-    NB: the "save" parameter references a DIRECTORY, not a file.
-    In that directory, there should be two files:
-    - model.pth (model state dict)
-    - valid_indices.pth (a list of indices corresponding to the validation set).
-
-    --data (str) - path to directory where data should be loaded from/downloaded
-    --save (str) - directory with necessary files (see above)
-    """
     fire.Fire(demo)
